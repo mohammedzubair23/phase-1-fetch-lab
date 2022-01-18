540a2d005e77ea647f5f46a636343cd9fdc725c2
Mohammed Zubair <mohammedzubair10@yahoo.com>
2022-01-18 16:47:02 -0500

    commit tree e4fc1d9822041e03b294e92ebf5394c6853c798f

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  return fetch('https://anapioficeandfire.com/api/books') 
+  .then(response => response.json())
+  .then(json => renderBooks(json))
   
 }
 
@@ -9,9 +12,10 @@ function renderBooks(books) {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
+    
   });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+   fetchBooks();
 });
